daea99b7f02b6d793769ab2246387f5f031ed217
patilkap <38049849+patilkap@users.noreply.github.com>
2018-05-31 12:29:53 -0700

    Constructed all functions
    Added time functions and completed basic funcs.

--- header.h
+++ header.h
@@ -13,62 +13,80 @@ class Supervisor;
 //Base class
 class User {
 private:
-    string name;
-    unsigned int id;
-    vector<Shift*> userhours;
-    friend Supervisor;
+	string name;
+	unsigned int id;
+	vector<Shift> userhours;
+	friend Supervisor;
 public:
-    User();
-    User(unsigned int num, string a);
-    void clockin();
-    void clockout();
-    void viewhours() const;
+	User();
+	User(unsigned int num, string a);
+	void clockin();
+	void clockout();
+	void viewhours() const;
 };
 
- //Derived class
+//Derived class
 class Supervisor : public User {
 public:
-    Supervisor(unsigned int num, string a) : User(num, a) {userhours[0];};
-    Supervisor() : User(111111, "N/A") {userhours[0];};
-    void viewemployeehours(User*);
+	Supervisor(unsigned int num, string a) : User(num, a) { userhours[0]; };
+	Supervisor() : User(111111, "N/A") { userhours[0]; };
+	void viewemployeehours(User*);
 };
 
 //Shift class
 class Shift {
 private:
-    time_t start;
-    time_t end;
-    double total;
+	time_t start;
+	time_t end;
 public:
-    double sumitup();
-    
+	Shift(time_t start) : start(start) {}
+	double sumitup(); // TODO
+	friend User;
+
 };
 
 
 //******************** FUNCTIONS ************************
 
-User::User(){
-    name = "N/A";
-    id = 111111;
-    userhours[0];
+User::User() {
+	name = "N/A";
+	id = 111111;
+	userhours[0];
 }
 
-User::User(unsigned int num, string a){
-    id = num;
-    name = a;
-    userhours[0];
+User::User(unsigned int num, string a) {
+	id = num;
+	name = a;
+	userhours[0];
 }
 
-void User::clockin(){
-    new Shift a;
-    hoursworked.pushback(a);
-    a.start = time;
-    return;
+void User::clockin() {
+	time_t current_time = time(NULL);
+	Shift a(current_time);
+	userhours.emplace_back(move(a));
+	return;
 }
 
-void User::clockout(){
-    Shift* a = hoursworked.back();
-    a.end = time;
-    return;
+void User::clockout() {
+	time_t current_time = time(NULL);
+	Shift& a = userhours.back();
+	a.end = current_time;
+	a.sumitup();
+	return;
 }
 
+void User::viewhours() const{
+	cout << name << ":" << endl;
+	for (auto x : userhours) {
+		unsigned int n = 1;
+		double tot = 0;
+		cout << n << ") " << x.sumitup()<<endl;
+		tot += x.sumitup();
+		++n;
+	}
+	return;
+}
+
+double Shift::sumitup() {
+	return difftime(start, end);
+}
