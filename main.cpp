81fbe1d602fdaa7c9b9a030224c3bf63e7755399
patilkap <38049849+patilkap@users.noreply.github.com>
2018-06-04 17:17:39 -0700

    Updated Source file to match new functions
    Added sign up/log in functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
 #include "Header.h"
 
-int main(){
-    
-    User(123456, "Mary");
-   // Supervisor(321111, "Bob", "CEO");
-    
-    cout<<"done creating...";
-    
-    return 0;
-}
+int main() {
 
+	
+	Supervisor sup(321111, "Bob");
+	User* emp1 = new User(123456, "Mary");
+	sup.addemp(emp1);
+
+		cout << "done creating...";
+		cout << "Enter 0 to sign up. Press any other key to sign in" << endl;
+		int q;
+		cin >> q;
+		if (q == 0) {
+			//ADD EMPLOYEE
+			unsigned int id;
+			cout << "Enter new ID#...";
+			cin >> id;
+			if (sup.findemployee(id) == NULL) {
+				cout << endl << "Thank you." << endl << endl << "Please enter employee name...";
+				string nam;
+				cin >> nam;
+				sup.addemp(new User(id, nam));
+			}
+		}
+		else {
+			//SIGN IN (RUN FIND EMPLOYEE)
+			unsigned int id;
+			User* loggedin;
+			cout << "Please enter your ID#...";
+			cin >> id;
+			loggedin = sup.findemployee(id);
+			if (loggedin == NULL) { cout << "Unable to find employee. Please enter your ID#..." };
+		}
+
+
+	return 0;
+
+}
