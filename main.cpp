efd23f5783a830ec8f815ed7fdf70f85bda9e033
patilkap <38049849+patilkap@users.noreply.github.com>
2018-06-07 10:38:26 -0700

    Update main with new functions
    Added sign up/log in and choices

--- main.cpp
+++ main.cpp
@@ -10,29 +10,46 @@ int main() {
 		cout << "done creating...";
 		cout << "Enter 0 to sign up. Press any other key to sign in" << endl;
 		int q;
-		unsigned int id;
 		cin >> q;
+	
+		//ADD EMPLOYEE
 		if (q == 0) {
-			//ADD EMPLOYEE
+			unsigned int id;
+			do {
 			cout << "Enter new ID#...";
 			cin >> id;
-			if (sup.findemployee(id) == NULL) {
+			if (sup.findemployee(id) != NULL) {
+				cout << "That ID# is already taken. Please try again with a different ID#." << endl;
+			}
+			} while (sup.findemployee(id) != NULL);
+
+			if(sup.findemployee==NULL){
 				cout << endl << "Thank you." << endl << endl << "Please enter employee name...";
 				string nam;
 				cin >> nam;
 				sup.addemp(new User(id, nam));
 			}
-			else { cout << "That ID# is already taken. Please try again with a different ID#." << endl; }
 		}
+
+		//SIGN IN (RUN FIND EMPLOYEE)
 		else {
-			//SIGN IN (RUN FIND EMPLOYEE)
+			unsigned int id;
 			User* loggedin;
 			cout << "Please enter your ID#...";
 			cin >> id;
 			loggedin = sup.findemployee(id);
-			if (loggedin == NULL) { cout << "Unable to find employee. Please enter your ID#..." };
-		}
+			if (loggedin == NULL) { cout << "Unable to find employee. Please enter your ID#..."; }
 
+			cout << endl<< "Welcome " << loggedin->getname() << "! Please select from the following options:"<<endl;
+			int choice;
+			do {
+				cout << "To clock in, press 0.   To clock out, press 1.   To view your hours, press 2.";
+				if (choice == 0) { loggedin->clockin(); }
+				else if (choice == 1) { loggedin->clockout(); }
+				else if (choice == 2) { loggedin->viewhours(); }
+				else { cout << "Invalid choice."; }
+			} while (choice != 0 || choice != 1 || choice != 2);
+		}
 
 	return 0;
 
