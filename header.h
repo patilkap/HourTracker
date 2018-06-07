b2dd0dc0f8c7092d2d73ce3c98fab948fa51d973
patilkap <38049849+patilkap@users.noreply.github.com>
2018-06-07 12:13:24 -0700

    Changed auto:x types to normal for loops
    For some reason auto : x didn't work for one of the functions so I replaced them with normal for loop.

--- header.h
+++ header.h
@@ -1,10 +1,20 @@
 #include <iostream>
 
+
+
 #include <string>
+
+
 
 #include <vector>
 
+
+
 #include <ctime>
+
+
+
+
 
 
 
@@ -12,63 +22,124 @@ using namespace std;
 
 
 
+
+
+
+
 //******************** CLASSES ************************
+
+
+
+
 
 
 
 class Shift;
 
+
+
 class Supervisor;
 
 
 
+
+
+
+
 //Base class
+
 class User {
 
+
+
 private:
+
+
 
 	string name;
 
+
+
 	unsigned int id;
+
+
 
 	vector<Shift> userhours;
 
+
+
 	friend Supervisor;
 
+
+
 public:
+
+
 
 	User();
 
+
+
 	User(unsigned int num, string a);
+
+
 
 	void clockin();
 
+
+
 	void clockout();
+
+
 
 	void viewhours() const;
 
+
+
 	string getname() { return name; }
-	
+
+
+
+
 
 };
+
+
 
 //Derived class
+
 class Supervisor : public User {
+
 private:
+
 	vector<User*> employees;
+
 public:
 
-	Supervisor(unsigned int num, string a) : User(num, a) { userhours[0]; };
+
+
+	Supervisor(unsigned int num, string a) : User(num, a) { };
+
+
 
 	Supervisor() : User(111111, "N/A") { userhours[0]; };
+
 	void addemp(User*);
+
 	User* findemployee(unsigned int b);
+
 	void viewemployeehours(unsigned int a);
+
+
 
 };
 
+
+
 //Shift class
+
 class Shift {
+
 
 private:
 
@@ -85,11 +156,11 @@ public:
 	friend User;
 
 
-
 };
 
-//******************** FUNCTIONS ************************
 
+
+//******************** FUNCTIONS ************************
 
 
 User::User() {
@@ -97,14 +168,12 @@ User::User() {
 	name = "N/A";
 
 	id = 111111;
-
-	userhours[0];
-
+	
 }
 
 
-
 User::User(unsigned int num, string a) {
+
 
 	id = num;
 
@@ -114,87 +183,152 @@ User::User(unsigned int num, string a) {
 
 
 
+
+
+
+
 void User::clockin() {
+
+
 
 	time_t current_time = time(NULL);
 
+
+
 	Shift a(current_time);
+
+
 
 	userhours.emplace_back(move(a));
 
+
+
 	return;
 
-}
 
+
+}
 
 
 void User::clockout() {
 
+
+
 	time_t current_time = time(NULL);
+
+
 
 	Shift& a = userhours.back();
 
+
+
 	a.end = current_time;
+
+
 
 	a.sumitup();
 
+
+
 	return;
 
+
+
 }
-
-
 
 void User::viewhours() const {
 
+
+
 	cout << name << ":" << endl;
 
-	for (auto x : userhours) {
+	double tot = 0;
 
-		unsigned int n = 1;
+	for (int i = 0; i < userhours.size();++i) {
 
-		double tot = 0;
 
-		cout << n << ") " << x.sumitup() << endl;
 
-		tot += x.sumitup();
 
-		++n;
+		cout << i+1 << ") " << userhours[i].sumitup() << endl;
+
+
+
+		tot += userhours[i].sumitup();
+
 
 	}
+
+	cout << endl << "Your total number of hours is: " << tot <<" hrs."<< endl;
+
 
 	return;
 
+
+
 }
+
+
 
 void Supervisor::addemp(User* u) {
+
 	employees.push_back(u);
+
 }
+
+
 
 User* Supervisor::findemployee(unsigned int b) {
+
 	User* t;
-	for (auto x : employees) {
-		if (x->id == b) {
-			t = x;
+
+	for (int i = 0; i < employees.size();++i) {
+
+		if (employees[i]->id == b) {
+
+			t = employees[i];
+
 		}
-		else { return NULL; }
+
+		else { t= NULL; }
+
 	}
+
 	return t;
+
 }
 
+
+
 void Supervisor::viewemployeehours(unsigned int a) {
+
 	User* emp = findemployee(a);
+
 	emp->viewhours();
+
 }
+
+
+
 
 
 double Shift::sumitup() {
 
+
+
 	double seconds, minutes;
+
+
 
 	seconds = end - start;
 
+
+
 	minutes = seconds / 60;
 
+
+
 	return minutes / 60;
+
+
 
 }
